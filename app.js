81c5168a10f9b03cd5f6ad06fa6d9538c6fd9d2b
Kit Kogan <kitkogan@users.noreply.github.com>
2019-11-21 20:32:44 -0600

    debug routes addition

--- app.js
+++ app.js
@@ -4,39 +4,52 @@ const express = require('express');
 const path = require('path');
 
 // add required data
-const { projects } = require('./data/data.json');
+const data = require('./data/data.json').data;
+const projectData = data.projects
 
-
-// add app routes
+// app route
 const app = express();
-const router = express.Router();
-
-const indexRouter = require('./routes/index');
-const aboutRouter = require('./routes/about');
-const projectRouter = require('./routes/project');
 
 // set view engine to pug
+
+
 app.set('view engine', 'pug');
 
 // set static route to serve static files in public folder
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-// set page routes
-app.use('/', indexRouter);
-app.use('/about', aboutRouter);
-app.use('/project', projectRouter);
+// set "home" route
+app.get('/', (req, res) => {
+    res.locals.projects = projectData;
+    res.render('index', {projects: res.locals.projects});
+});
+
+// set "about" route
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+// set "project" routes
+app.get('/projects/:id', (req, res) => {
+    const query = req.params.id
+    res.locals.project = projectData[query]
+    res.render('project')
+});
+
+// add static css routes
+app.use('/static/css', express.static('public'))
 
 // add middleware for catching errors
 app.use((req, res, next) => {
-    const err = new Error('Uh-oh! Page not found!');
+    const err = new Error('Uh-oh! Page not found!')
+    res.locals.error = err
     err.status = 404;
-    next('err');
+    res.render('error')
 });
 
 app.use((err, req, res, next) => {
     res.locals.error = err
-    res.status(err.status);
-    console.log(err.message, err.status, err.stack);
+    console.log(`Sorry, we seem to have encountered an error!: ${err.status}`);
     res.render('error')
 });
 
--- routes/project.js
+++ routes/project.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { projects } = require('..data/data.json');
+const { projects } = require('.data/data.json');
 
 router.get('/:id', (req, res, next) => {
     let { id } = req.params;
