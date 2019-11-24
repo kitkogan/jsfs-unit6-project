f96e4787164368c1ce9539bc725f77efb1cb62df
Kit Kogan <kitkogan@users.noreply.github.com>
2019-11-23 22:51:33 -0600

    update and debug routes

--- app.js
+++ app.js
@@ -1,59 +1,54 @@
 
 // add required packages
 const express = require('express');
-const path = require('path');
-
-// add required data
-const data = require('./data/data.json').data;
-const projectData = data.projects
-
-// app route
 const app = express();
+const router = express.Router();
+const path = require('path');
+const { projects } = require('./data/data.json');
+
+const indexRouter = require('./routes/index');
+const aboutRouter = require('./routes/about');
+const projectRouter = require('./routes/project');
 
 // set view engine to pug
-
-
 app.set('view engine', 'pug');
 
 // set static route to serve static files in public folder
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 // set "home" route
-app.get('/', (req, res) => {
-    res.locals.projects = projectData;
-    res.render('index', {projects: res.locals.projects});
-});
+app.use('/', indexRouter);
+
 
 // set "about" route
-app.get('/about', (req, res) => {
-    res.render('about');
-});
+app.use('/about', aboutRouter);
 
 // set "project" routes
-app.get('/projects/:id', (req, res) => {
-    const query = req.params.id
-    res.locals.project = projectData[query]
-    res.render('project')
-});
-
-// add static css routes
-app.use('/static/css', express.static('public'))
+app.use('/projects', projectRouter);
 
 // add middleware for catching errors
 app.use((req, res, next) => {
-    const err = new Error('Uh-oh! Page not found!')
-    res.locals.error = err
+    console.error("Uh-oh! Page not found");
+    const err = new Error ('Not Found');
     err.status = 404;
-    res.render('error')
+    next(err);
+});
+
+app.use((req, res, next) => {
+    const err = new Error ('Oops! An error occured');
+    console.error("An Error Occured");
+    err.status = 500;
+    next(err);
 });
 
 app.use((err, req, res, next) => {
-    res.locals.error = err
-    console.log(`Sorry, we seem to have encountered an error!: ${err.status}`);
-    res.render('error')
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
+
 
 // set listen on port 3000
 app.listen(3000, () => {
-    console.log("Port 3000 is working!")
+    console.log('app is running on local host 3000')
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const app = require('../data/data.json')
+const app = require('../app.js');
+const { projects } = require('../data/data.json');
 
 router.get('/', function(req, res, next) {
     res.render('index', { projects });
