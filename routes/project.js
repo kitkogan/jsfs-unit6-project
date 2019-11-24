5f25245c9887b98b4d5350fe5f7acdcd4cdc5da2
Kit Kogan <kitkogan@users.noreply.github.com>
2019-11-23 21:15:39 -0600

    revert to previous version

--- routes/project.js
+++ routes/project.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { projects } = require('.data/data.json');
+const { projects } = require('../data/data.json');
 
 router.get('/:id', (req, res, next) => {
     let { id } = req.params;
     if(id >= projects.length) {
         next();
     }
-    
+
     const projectName = projects[id].project_name;
 
-    const { technologies, image_urls, description, github_link, live_link } = projects[id];
+    const { technologies, image_urls, description, github_link, live_link } =  projects[id];
     const projectInfo = { projectName, technologies, image_urls, description, github_link, live_link };
     return res.render('project', { projectInfo });
-
 });
 
 module.exports = router;
